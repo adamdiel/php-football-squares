7b360b02c57b59195be94e76d4baf23c7374f1f0
Adam Diel <adamdiel@github.com>
2020-11-19 15:58:11 -0700

    Modified spacing
    The original code didn't look great on vscode. Not sure if that was proper php formatting or it was put through some sort of minimify tool.

--- classes/football_squares.class.php
+++ classes/football_squares.class.php
@@ -10,57 +10,47 @@ class football_squares{
 		public $team_two = 'Team Two';
 		public $price = '5.00';
 		public $currency_symbol = '$';
-		function __construct(){
-						
-			
-		//error_reporting(E_ALL);
-		//ini_set('display_errors', '1');
-		session_start();
-		
 
-			
-			if($_GET['logout'] == 1){
+		function __construct(){
+			//error_report(E_ALL);
+			//ini_set('display_errors', '1');
+
+			session_start();
+
+			if($_GET['logout'] ==1){
 				$exp = time() + (86400 * 30);
-			setcookie('auth', '', -$exp);
-			header("Location: index.php");	
+				setcookie('auth', '', -$exp);
+				header("Location: index.php");
 			}
 		}
 		
 	
 		function write($id,$value){
-			
+
 			$data = $this->data();
-			
 			$data[$id] = $value;
-			
-			
 			
 			$write = serialize($data);
 			file_put_contents($this->data, $write);
-			
-			
-		}
-		function data(){
-		 $file = $this->data;
-    		
-					
-		if (file_exists( $file)) {
-			$data = file_get_contents( $file);
-		} else {
-			file_put_contents($file, serialize(array()));
-			$data = file_get_contents( $file);
-		}
-			
-		
-  
- 		 $array = unserialize($data);
-		 
-		 return $array;
-			
-			
-		}
-		
 
+		}
+
+		function data(){
+
+			$file = $this->data;
+					
+			if (file_exists( $file)) {
+				$data = file_get_contents( $file);
+			} else {
+				file_put_contents($file, serialize(array()));
+				$data = file_get_contents( $file);
+			}
+
+			$array = unserialize($data);
+		 
+			return $array;
+			
+		}
 		
 		function form($id,$entry = ''){
 			
@@ -70,13 +60,15 @@ class football_squares{
 				$value = $_SESSION['name'] ;
 			}
 			
-			$h .='<div style="display:none"><div class="register_square_'.$id.'"><form action="index.php" method="post">
-			<input type="hidden" name="id" value="'.$id.'">
-				  Name: <input type="text" name="name" value="'.$value.'" style="width:200px"> <input type="submit" name="save" value="Save it!">
-				  </form></div></div>';
+			$h .=
+				'<div style="display:none"><div class="register_square_'.$id.'"><form action="index.php" method="post">
+				<input type="hidden" name="id" value="'.$id.'">
+				Name: <input type="text" name="name" value="'.$value.'" style="width:200px"> <input type="submit" name="save" value="Save it!">
+				</form></div></div>';
 				  
-				  return $h;
+			return $h;
 		}
+
 		function build(){
 				
 				
@@ -91,65 +83,66 @@ class football_squares{
 				$data = $this->data();
 				
 				
-				$h .='<style type="text/css">
+				$h .=
+				'<style type="text/css">
 				.square_col{width:'.$columns.'%}
-					  </style>
-					  <div id="team_one">'.$this->team_one.'</div>
-						<div id="team_two">'.$this->team_two.'</div>
-					  
-					  <div id="squares_container">';
-						$abs = 0;
+				</style>
+				<div id="team_one">'.$this->team_one.'</div>
+				<div id="team_two">'.$this->team_two.'</div>
+				<div id="squares_container">';
+				$abs = 0;
 					
 					
-					for($i=0; $i< $this->rows; $i++){
-						if($i != 0){
+				for($i=0; $i< $this->rows; $i++){
+					if($i != 0){
 						$abs += $this->cols;
-						}
-						$cols =0;
-						$h .= '<div class="square_row">';
-							for($c=0; $c< $this->cols; $c++){
-								$cols++;
-								$num = $cols + $abs;
+					}
+					$cols =0;
+					$h .= '<div class="square_row">';
+					for($c=0; $c< $this->cols; $c++){
+						$cols++;
+						$num = $cols + $abs;
 								
-								if($data[$num] == ''){
-								$link = '<a href="javascript:squares_popup(\'.register_square_'.$num.'\')" >Register <br>#'.$num.'</a>';	
-								}else{
-									
-									if($this->auth() == true){
-									 $picked = 'javascript:squares_popup(\'.register_square_'.$num.'\')';
+						if($data[$num] == ''){
+							$link = '<a href="javascript:squares_popup(\'.register_square_'.$num.'\')" >Register <br>#'.$num.'</a>';	
+						}else{
+							if($this->auth() == true){
+								$picked = 'javascript:squares_popup(\'.register_square_'.$num.'\')';
 									 
-									 if($data[$num] != ''){
-										 $entry  = $data[$num];
-									 }
-									}else{
-									 $picked = '#';	
-										$entry  = '';
-									}
-									
-									
-								$link = '<a href="'.$picked.'" class="chosen">'.$data[$num].'</a>';	
+								if($data[$num] != ''){
+									$entry  = $data[$num];
 								}
+							}else{
+								$picked = '#';	
+								$entry  = '';
+							}
+									
+									
+							$link = '<a href="'.$picked.'" class="chosen">'.$data[$num].'</a>';	
+						}
 								
-								$h .= '<div class="square_col">
+						$h .= 
+								'<div class="square_col">
 								<div class="square_col_inside">
 								'.$link .'
 								'.$this->form($num,$entry).'
 								</div> 
 								</div>';
-							}
-						
-						$h .='<div style="clear:both"></div></div>';
-						
 					}
-					$h .= '</div>';
+						
+					$h .='<div style="clear:both"></div></div>';
+						
+				}
+				$h .= '</div>';
 					
-					$h .= $this->admin();
-					$h .= $this->stat_data();
+				$h .= $this->admin();
+				$h .= $this->stat_data();
+
 				return $h;			
 		}
 		
 		
-				function stat_data(){
+		function stat_data(){
 			
 			$data = $this->data();
 			$total = count($data);
@@ -164,22 +157,19 @@ class football_squares{
 			}
 			$total_squares .='</ul>';
 			
-			$h .='<div class="noPrint">
-			  <script>
-  $(function() {
-    $( "#progressbar" ).progressbar({
-      value: '.$total .'
-    });
-  });
-  </script>
-
-			
+			$h .=
+			'<div class="noPrint">
+			<script>
+  				$(function() {
+    				$( "#progressbar" ).progressbar({
+      					value: '.$total .'
+    				});
+  				});
+			</script> 
 			<h2>Stats</h2>
 			<div id="progressbar"><div class="progress-label">'.$total .'% Complete, only '.$left.' squares left. </div></div>	
 			'.$total_squares.'	
-				
-				</div>
-			';
+			</div>';
 			
 			return $h;
 		}
@@ -196,17 +186,17 @@ class football_squares{
 				}
 			
 		}
+
 		function admin(){
-				$password = $this->sanitize($_POST['password']);
-		if($_POST['password'] != ''){
+			$password = $this->sanitize($_POST['password']);
+			if($_POST['password'] != ''){
 			
 				if($_POST['password']  == $this->password){
-				
-				$exp = time() + (86400 * 30);
-				setcookie('auth', md5($_POST['password']), $exp);
-				header("Location: index.php");
+					$exp = time() + (86400 * 30);
+					setcookie('auth', md5($_POST['password']), $exp);
+					header("Location: index.php");
 				}else{
-				$h .= 'Inccorect Login';
+					$h .= 'Inccorect Login';
 				}
 				
 			}
@@ -221,23 +211,20 @@ class football_squares{
 				$h .='<a href="?logout=1" class="button">Logout</a>';
 			}
 					
-					$h .='<div style="display:none"><div class="admin_login"><form action="index.php" method="post">
-			
-				  Password: <input type="text" name="password"  style="width:200px"> <input  type="submit" name="login" value="Login">
-				  </form></div></div>
+			$h .=
+			'<div style="display:none"><div class="admin_login"><form action="index.php" method="post">
+			Password: <input type="text" name="password"  style="width:200px"> <input  type="submit" name="login" value="Login">
+			</form></div></div>
+			</div>';
 					
-					</div>';
-					
-			
-			
 			return $h;
 			
 		}
 	
 			
-			function sanitize($input) {
-				$output = addslashes(htmlspecialchars(strip_tags(trim($input))));
-				return $output;
-			}
+		function sanitize($input) {
+			$output = addslashes(htmlspecialchars(strip_tags(trim($input))));
+			return $output;
+		}
 }
 ?>
